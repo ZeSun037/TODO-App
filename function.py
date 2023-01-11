f12dbd3ec8db6da1526b9ffae5355453d28e3793
Ze "Zack" Sun <zesun@ucsd.edu>
2023-01-11 14:05:27 -0800

    Edit & Delete Button implemented

--- function.py
+++ function.py
@@ -1,10 +1,18 @@
-def getListTodos(file='todos.txt'):
+import os
+
+file_path = 'todos.txt'
+
+mode = 'a' if os.path.exists(file_path) else 'w'
+with open(file_path, mode) as f:
+    pass
+
+def getListTodos(file=file_path):
     with open(file, 'r') as file_local:
         todos_local = file_local.readlines()
 
     return todos_local
 
 
-def writeListTodos(todos_arg, fileDest='todos.txt'):
-    with open(fileDest, 'wr') as file:
+def writeListTodos(todos_arg, fileDest=file_path):
+    with open(fileDest, 'w+') as file:
         file.writelines(todos_arg)
--- gui.py
+++ gui.py
@@ -6,10 +6,15 @@ from PySimpleGUI import __version__
 text = Text("Type in a Todo")
 input_box = InputText(tooltip="Enter a Todo", key='todo')
 add_button = Button("Add")
+list_box = Listbox(values=function.getListTodos(),
+                   key='todos', enable_events=True, size=[45, 10])
 
+edit_button = Button("Edit")
+
+delete_button = Button("Delete")
 
 window = Window("Your Todos", layout=[[text],
-                [input_box, add_button]],
+                [input_box, add_button, edit_button, delete_button], [list_box]],
                 font=('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -18,6 +23,31 @@ while True:
             todos = function.getListTodos()
             todos.append(values['todo'] + '\n')
             function.writeListTodos(todos)
+            window['todo'].update(value="")
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = function.getListTodos()
+            ind = todos.index(todo_to_edit)
+            todos[ind] = new_todo + '\n'
+            function.writeListTodos(todos)
+
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+        case 'Delete':
+            todo_to_delete = values['todo']
+            todos = function.getListTodos()
+
+            ind = todos.index(todo_to_delete)
+            if todos[ind]:
+                del todos[ind]
+
+            window['todo'].update(value="")
+            window['todos'].update(values=todos)
+
         case WIN_CLOSED:
             break
 
