db10a1494a310e4bb3f5c592dbb881269c2a94bb
Ze "Zack" Sun <zesun@ucsd.edu>
2023-01-11 08:06:58 -0800

    Add button implemented

--- gui.py
+++ gui.py
@@ -4,10 +4,21 @@ from PySimpleGUI import *
 from PySimpleGUI import __version__
 
 text = Text("Type in a Todo")
-input_box = InputText(tooltip="Enter a Todo")
+input_box = InputText(tooltip="Enter a Todo", key='todo')
 add_button = Button("Add")
 
 
-window = Window("Your Todos", layout=[[text], [input_box, add_button]])
-window.read()
+window = Window("Your Todos", layout=[[text],
+                [input_box, add_button]],
+                font=('Helvetica', 20))
+while True:
+    event, values = window.read()
+    match event:
+        case "Add":
+            todos = function.getListTodos()
+            todos.append(values['todo'] + '\n')
+            function.writeListTodos(todos)
+        case WIN_CLOSED:
+            break
+
 window.close()
