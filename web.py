2331d65b0d37db7a3390911be4ffc5e9ad175738
Ze "Zack" Sun <zesun@ucsd.edu>
2023-01-17 19:36:05 -0800

    Created web interface for the app.

--- web.py
+++ web.py
@@ -3,10 +3,23 @@ import functions
 
 todos = functions.get_todos()
 
+
+def add_todos():
+    new_todo = st.session_state["new_todo"] + "\n"
+    todos.append(new_todo)
+    functions.write_todos(todos)
+
+
 st.title("Your Todos...")
 st.subheader("Manage your todos here to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
-st.text_input(label="", placeholder="Enter a todo here.")
+st.text_input(label="", placeholder="Enter a todo here.",
+              on_change=add_todos, key="new_todo")
